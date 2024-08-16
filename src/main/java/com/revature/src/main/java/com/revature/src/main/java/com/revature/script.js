ed7386fc3fd420118b1adb8994c61143b126c213
samargitworld <124122212+samargitworld@users.noreply.github.com>
2024-08-16 13:13:04 +0530

    Create script.js

---
+++ src/main/java/com/revature/src/main/java/com/revature/src/main/java/com/revature/script.js
@@ -0,0 +1,79 @@
+// Task 1: Integer Datatype
+
+// TODO: Create an integer variable named 'myInteger' and assign it an integer value of 619
+function IntegerMethod() {
+
+    let outputElement = document.getElementById("output");
+    outputElement.textContent = "replace me with the variable you've created";
+    return "replace me with the variable you've created";
+}
+
+// Task 2: Float Datatype
+
+// TODO: Create a float variable named 'myFloat' and assign it a floating-point value of 3.14
+function FloatMethod() {
+
+    let outputElement = document.getElementById("output");
+    outputElement.textContent = "replace me with the variable you've created";
+    return "replace me with the variable you've created";
+}
+
+// Task 3: Boolean Datatype
+
+// TODO: Create a boolean variable named 'myBoolean' and assign it a value of false
+function BooleanMethod() {
+
+    let outputElement = document.getElementById("output");
+    outputElement.textContent = "replace me with the variable you've created";
+    return "replace me with the variable you've created";
+}
+
+// Task 4: String Datatype
+
+// TODO: Create a string variable named 'myString' and assign it a string value of "Just started learning JavaScript!".
+function StringMethod() {
+    string1 =  "Just started learning JavaScript!";
+    let outputElement = document.getElementById("output");
+    outputElement.textContent = "replace me with the variable you've created";
+    return "replace me with the variable you've created";
+}
+
+// Task 5: Tuple Datatype
+
+// TODO: Create a tuple variable named 'myTuple' and assign it a tuple value of 10, 20, 30.
+function TupleMethod() {
+
+    let outputElement = document.getElementById("output");
+    outputElement.textContent = JSON.stringify("replace me with the variable you've created");
+    return "replace me with the variable you've created";
+}
+
+// Task 6: List Datatype
+
+// TODO: Create a list variable named 'myList' and assign it a list value of 1, 2, 3, 4, 5.
+function ListMethod() {
+;
+    let outputElement = document.getElementById("output");
+    outputElement.textContent = "replace me with the variable you've created";
+    return "replace me with the variable you've created";
+}
+
+// Task 7: Set Datatype
+
+// TODO: Create a set variable named 'mySet' and assign it a set value of a, b, c.
+function SetMethod() {
+
+    let outputElement = document.getElementById("output");
+    outputElement.textContent = JSON.stringify("replace me with the variable you've created");
+    return "replace me with the variable you've created";
+}
+
+// Task 8: Dictionary Datatype
+
+// TODO: Create a dictionary variable named 'myDict' and assign it a dictionary value of key1: value1, key2: value2
+function DictionaryMethod() {
+
+    let outputElement = document.getElementById("output");
+    outputElement.textContent = JSON.stringify("replace me with the variable you've created");
+    return "replace me with the variable you've created";
+}
